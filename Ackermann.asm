b68844c14ce590dbbedbb88a7b3666a3192d3c5c
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-04-28 17:28:37 -0300

    Update Ackermann.asm

--- Ackermann.asm
+++ Ackermann.asm
@@ -1,92 +1,120 @@
-.globl main
 .text
+	.globl main
 
 main:
-	# Print m_message
+	# Print scan m
 	li $v0, 4
 	la $a0, m_message
 	syscall
 	
-	# Scan terminal "m"
+	# Scan terminal
 	li $v0, 5
 	syscall
 	move $s0, $v0
 	
-	# Print n_message
+	# Print scan n
 	li $v0, 4
 	la $a0, n_message
 	syscall
 	
-	# Scan terminal "n"
+	# Scan terminal
 	li $v0, 5
 	syscall
 	move $s1, $v0
 	
 	# Save in Stack
 	addi $sp, $sp, -12
-	sw $ra, 0($sp) # 0 - return address
+	sw $ra, 0($sp) # 0 - address
 	sw $s0, 4($sp) # 4 - m
 	sw $s1, 8($sp) # 8 - n
-
+	
 	# Jump and Link
 	jal ackermann
-
-	# Restore from Stack	
-	lw $s1, 8($sp) # 8 - n
-	lw $s0, 4($sp) # 4 - m
-	lw $ra, 0($sp) # 0 - return address
-	addi $sp, $sp, 12
-
-	# Result
-	move $s0, $v0 # armazena o resultado em $s0
+	
+	# Clear the Stack
+	add $sp, $sp, 12
+	
+	# Move ackermann result to s2
+	move $s2, $v0
+	
+	# Print results string
 	li $v0, 4
 	la $a0, results
 	syscall
-
+	
+	# Print int result
 	li $v0, 1
-	move $a0, $s0 # carrega o resultado para $a0
+	move $a0, $s2
 	syscall
 
-	li $v0, 10 # Encerra o programa
+end:
+	# Finish program
+	li $v0, 10
 	syscall
 	
 ackermann:
-	# Save registers
-	addi $sp, $sp, -12
-	sw $ra, 0($sp) # 0 - return address
-	sw $s0, 4($sp) # 4 - m
-	sw $s1, 8($sp) # 8 - n
 
-	# Check base cases
-	bne $s0, $zero, elseif
-	addi $v0, $s1, 1 # if m == 0, return n+1
-	j done
-
-	elseif:
-	bne $s1, $zero, else 
-	addi $s0, $s0, -1 # if n==0, m!=0, return m-1
-	li $s1, 1
-	jal ackermann # return ackermann(m=1,1)
-	j done
-
-	else:
-	addi $s1, $s1, -1 # n - 1
-	move $s2, $s0
-	jal ackermann # v0 = ackermann(m, n-1)
-	move $a0, $v0
-	move $a1, $s2
-	addi $s0, $s0, -1 # m - 1
-	jal ackermann # return ackermann(m-1, ackermann(m, n-1))
-
-	# Restore registers
-	done:
-	lw $ra, 8($sp) # 8 - return address
-	lw $s1, 4($sp) # 4 - n
-	lw $s0, 0($sp) # 0 - m
-	addi $sp, $sp, 12
+	# Load arguments
+	lw $s0, 4($sp) # m
+	lw $s1, 8($sp) # n
+	
+	# If m not equals 0
+	bne $s0, 0, mnotzero
+	addi $v0, $s1, 1
 	jr $ra
+	
+	mnotzero:
+	# If n not equals 0
+	bne $s1, 0, nnotzero
+	
+	# Create space in Stack and set data
+	addi $sp, $sp, -12
+	addi $t0, $s0, -1
+	li $t1, 1
+	
+	# Save in Stack
+	sw $ra, 0($sp) # 0 - address
+	sw $t0, 4($sp) # 4 - m - 1
+	sw $t1, 8($sp) # 8 - n = 1
+	
+	# Call ackerman
+	jal ackermann
+	
+	# Load ra and clear the stack
+	lw $ra, 0($sp)
+	addi $sp, $sp, 12
+	
+	# Return
+	jr $ra
+	
+	nnotzero:
+	
+	addi $t0, $s1, -1 # n - 1
+	
+	addi $sp, $sp, -12
+	sw $ra, 0($sp)
+	sw $s0, 4($sp) # m
+	sw $t0, 8($sp) # n - 1
+	
+	jal ackermann
+	
+	move $t1, $v0 # n
+	lw $t0, 4($sp)
+	addi $t0, $t0, -1 # m
+	
+	sw $t0, 4($sp) # m - 1
+	sw $t1, 8($sp) # return
+	
+	jal ackermann
+	
+	lw $ra 0($sp)
+	addi $sp, $sp, 12
+	
+	jr $ra
+	
 
 .data
-m_message: .asciiz "Digite o valor m: "
+
+m_message: .asciiz "Digite o valor m ou -1 para abortar a execução:\n "
 n_message: .asciiz "Digite o valor n: "
 results: .asciiz "Resultado: "
